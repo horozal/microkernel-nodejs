ad65af2ef49c3e265f08010290b07be1f54a6625
Feryal Fulya Horozal <horozal@atb-bremen.de>
2023-06-07 17:56:45 +0200

    Update file

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const Core = require("packages/core/src/index.js");
 const Module = require("packages/modules/module/module.js");
 
-module.exports = class Microkernel {
+class Microkernel {
+  constructor() {
+    this.core = new Core();
+    this.module = new Module();
+  }
 
+  sayHello() {
+    return "Hello, World!";
+  }
 }
+
+// Usage
+const microkernel = new Microkernel();
+console.log(microkernel.sayHello());
